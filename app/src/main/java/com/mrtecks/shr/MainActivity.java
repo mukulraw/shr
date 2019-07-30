f9cdf0a0b9b6c246194de103ff9aca890d43d393
mukul <mukulraw199517@gmail.com>
2019-07-31 03:25:33 +0530

    statrted

--- app/src/main/java/com/mrtecks/shr/MainActivity.java
+++ app/src/main/java/com/mrtecks/shr/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mrtecks.shr;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.http.SslCertificate;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -21,17 +25,55 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
 
         mWebView = findViewById(R.id.webview);
         mWebView.setListener(this, this);
+        mWebView.getSettings().setAppCacheEnabled(false);
         mWebView.loadUrl("https://www.mysuperhumanrace.com/");
-        mWebView.setMixedContentAllowed(false);
         mWebView.setGeolocationEnabled(true);
-        mWebView.clearSslPreferences();
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+
         mWebView.setWebViewClient(new WebViewClient()
         {
             @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
+            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                String message = "SSL Certificate error.";
+                switch (error.getPrimaryError()) {
+                    case SslError.SSL_UNTRUSTED:
+                        message = "The certificate authority is not trusted.";
+                        break;
+                    case SslError.SSL_EXPIRED:
+                        message = "The certificate has expired.";
+                        break;
+                    case SslError.SSL_IDMISMATCH:
+                        message = "The certificate Hostname mismatch.";
+                        break;
+                    case SslError.SSL_NOTYETVALID:
+                        message = "The certificate is not yet valid.";
+                        break;
+                }
+                message += " Do you want to continue anyway?";
+
+                builder.setTitle("SSL Certificate Error");
+                builder.setMessage(message);
+                builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.proceed();
+                    }
+                });
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.cancel();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
+
+
     }
 
     @Override
