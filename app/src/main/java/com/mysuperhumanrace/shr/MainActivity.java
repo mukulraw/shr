63224de49742f31abbf31281cbef56e7c26e634a
mukulshr <mukul@mysuperhumanrace.com>
2022-08-03 17:42:37 +0530

    VAPT done

--- app/src/main/java/com/mysuperhumanrace/shr/MainActivity.java
+++ app/src/main/java/com/mysuperhumanrace/shr/MainActivity.java
@@ -8,18 +8,27 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
 import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.util.Locale;
+
 import im.delight.android.webview.AdvancedWebView;
 
-public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener{
+public class MainActivity extends AppCompatActivity implements AdvancedWebView.Listener {
     private AdvancedWebView mWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CookieManager cookieManager = CookieManager.getInstance();
 
         mWebView = findViewById(R.id.webview);
         mWebView.setListener(this, this);
@@ -30,8 +39,36 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
         mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
 
-        mWebView.setWebViewClient(new WebViewClient()
-        {
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                if (url.contains("https://www.mysuperhumanrace.com/login")) {
+                    //Log.d("login", url);
+                    /*Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finishAffinity();*/
+                    return true;
+                }
+                else if(url.contains("https://www.mysuperhumanrace.com/logout"))
+                {
+                    //Log.d("logout", url);
+                    cookieManager.removeAllCookies(new ValueCallback<Boolean>() {
+                        // a callback which is executed when the cookies have been removed
+                        @Override
+                        public void onReceiveValue(Boolean aBoolean) {
+                            Log.d("cookiex", "Cookie removed: " + aBoolean);
+                        }
+                    });
+                    WebStorage.getInstance().deleteAllData();
+                    mWebView.clearCache(true);
+                    mWebView.clearHistory();
+                }
+
+                return false;
+            }
+
             @Override
             public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
@@ -90,7 +127,6 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
     }
 
 
-
     @Override
     public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
 
@@ -132,7 +168,9 @@ public class MainActivity extends AppCompatActivity implements AdvancedWebView.L
 
     @Override
     public void onBackPressed() {
-        if (!mWebView.onBackPressed()) { return; }
+        if (!mWebView.onBackPressed()) {
+            return;
+        }
         // ...
         super.onBackPressed();
     }
